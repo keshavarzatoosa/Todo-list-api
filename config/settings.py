54a5a78eb320707ba5acc3616f0b8e198152f5e6
keshavarzatoosa.dev <hel_sok_tikay@yahoo.com>
2025-01-29 02:32:15 -0800

    add drf-yasg to project

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'drf_yasg',
     'users.apps.UsersConfig',
     'api.apps.ApiConfig',
     'todos.apps.TodosConfig',
