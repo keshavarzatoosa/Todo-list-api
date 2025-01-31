d00c6d1e9c4d3f5074c418e6b4dda2fcef34c394
keshavarzatoosa.dev <hel_sok_tikay@yahoo.com>
2025-01-31 14:12:13 -0800

    add CustomUserTests for test create_user and create_superuser methods

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+User = get_user_model()
+
+
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        user = User.objects.create_user(
+           email='test@example.com',
+           name='Test User',
+           password='testpassword123' 
+        )
+        self.assertEqual(user.email, 'test@example.com')
+        self.assertEqual(user.name, 'Test User')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        admin_user = User.objects.create_superuser(
+           email='admin@example.com',
+           name='Admin User',
+           password='adminpassword123' 
+        )
+        self.assertEqual(admin_user.email, 'admin@example.com')
+        self.assertEqual(admin_user.name, 'Admin User')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
+    
+    def test_create_user_without_email(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_user(
+                email='',
+                name='Test User',
+                password='testpassword123'
+            )
+    
+    def test_create_superuser_without_is_staff(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='admin@example.com',
+                name='Admin User',
+                password='adminpassword123',
+                is_staff=False
+            )
+    
+    def test_create_superuser_without_is_superuser(self):
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='admin@example.com',
+                name='Admin User',
+                password='adminpassword123',
+                is_superuser=False 
+            )
